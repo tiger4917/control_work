7920b6a3c20f8ea4dc227c751a9744ea018779e5
tiger4917 <tiger491775@gmail.com>
2022-08-19 15:07:51 +0300

    добавил код решения

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[]array = {"1234", "asdf", "ok!", ":-)"};
+string[]newArray = new string[4];
+int i = 0; int j = 0;
+while(i < array.Length)
+{
+    if (array[i].Length <=3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+    i++;
+}
+Console.Write("["+ string.Join(", ",array) +"]");
+Console.WriteLine();
+Console.Write("[" + string.Join(", ",newArray) + "]");
